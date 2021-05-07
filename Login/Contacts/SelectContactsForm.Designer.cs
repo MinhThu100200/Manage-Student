2e7f7e576baffbf76e9fc2513d474b868d48524b
MinhThu100200 <18128062@student.hcmute.edu.vn>
2021-05-07 22:28:16 +0700

    update

--- Login/Contacts/SelectContactsForm.Designer.cs
+++ Login/Contacts/SelectContactsForm.Designer.cs
@@ -29,12 +29,37 @@ namespace Login
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridViewContact = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewContact)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewContact
+            // 
+            this.dataGridViewContact.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridViewContact.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewContact.Location = new System.Drawing.Point(12, 52);
+            this.dataGridViewContact.Name = "dataGridViewContact";
+            this.dataGridViewContact.RowHeadersWidth = 51;
+            this.dataGridViewContact.RowTemplate.Height = 26;
+            this.dataGridViewContact.Size = new System.Drawing.Size(776, 386);
+            this.dataGridViewContact.TabIndex = 0;
+            // 
+            // SelectContactsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Plum;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridViewContact);
+            this.Name = "SelectContactsForm";
             this.Text = "SelectContactsForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewContact)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewContact;
     }
 }
--- Login/Contacts/ShowFullListContactsForm.Designer.cs
+++ Login/Contacts/ShowFullListContactsForm.Designer.cs
@@ -29,19 +29,77 @@ namespace Login
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridViewContacts = new System.Windows.Forms.DataGridView();
+            this.listBoxNameGroup = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewContacts)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridViewContacts
+            // 
+            this.dataGridViewContacts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewContacts.Location = new System.Drawing.Point(385, 66);
+            this.dataGridViewContacts.Name = "dataGridViewContacts";
+            this.dataGridViewContacts.RowHeadersWidth = 51;
+            this.dataGridViewContacts.RowTemplate.Height = 26;
+            this.dataGridViewContacts.Size = new System.Drawing.Size(835, 404);
+            this.dataGridViewContacts.TabIndex = 0;
+            // 
+            // listBoxNameGroup
+            // 
+            this.listBoxNameGroup.FormattingEnabled = true;
+            this.listBoxNameGroup.ItemHeight = 16;
+            this.listBoxNameGroup.Location = new System.Drawing.Point(12, 66);
+            this.listBoxNameGroup.Name = "listBoxNameGroup";
+            this.listBoxNameGroup.Size = new System.Drawing.Size(367, 404);
+            this.listBoxNameGroup.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 13F, System.Drawing.FontStyle.Underline);
+            this.label1.ForeColor = System.Drawing.Color.DarkOrange;
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 27);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Group";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 13F, System.Drawing.FontStyle.Underline);
+            this.label2.ForeColor = System.Drawing.Color.DarkOrange;
+            this.label2.Location = new System.Drawing.Point(382, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 27);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Contact";
             // 
             // ShowFullListContactsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1103, 477);
+            this.BackColor = System.Drawing.Color.Purple;
+            this.ClientSize = new System.Drawing.Size(1232, 477);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBoxNameGroup);
+            this.Controls.Add(this.dataGridViewContacts);
             this.Name = "ShowFullListContactsForm";
             this.Text = "ShowFullListContactsForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewContacts)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewContacts;
+        private System.Windows.Forms.ListBox listBoxNameGroup;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
